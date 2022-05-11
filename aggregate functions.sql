335b9136de7dd2ba9e16c7382c3c35dfae5606f9
jefkadhammananda <104891582+jefkadhammananda@users.noreply.github.com>
2022-05-11 16:51:54 +0700

    Add files via upload

---
+++ aggregate functions.sql
@@ -0,0 +1,48 @@
+
+--melakukan perhitungan jumlah apps pada tabel fake_apps yang memiliki nilai price 0
+
+select count(*)
+from fake_apps
+where price =0;
+
+--melakukan perhitungan total jumlah downloads apps 
+
+select sum(downloads)
+from fake_apps;
+
+--melakukan pemilihan apps dengan harga termahal
+
+select max(price)
+from fake_apps;
+
+--melakukan perhitungan rata-rata harga apps
+
+select avg(price)
+from fake_apps;
+
+--melakukan perhitungan rata rata harga apps dan membulatkan nilainya dengan pembulatan 2 angka dibelakang koma
+
+select round (avg (price),2)
+from fake_apps;
+
+--melakukan perhitungan total jumlah downloads terhadap setiap category apps
+
+select category, sum(downloads)
+from fake_apps
+group by category;
+
+--melakukan perhitungan rata-rata downloads terhadap setiap category apps dan terhadap harga yang ada pada setiap category apps
+
+select category, 
+   price,
+   avg(downloads)
+from fake_apps
+group by 1,2;
+
+--melakukan select data terhadap price, rata-rata downloads dengan pembulatan, dan jumlah apps pada setiap price yang ada dengan ketentuan jumlah apps harus > 10
+
+select price,
+round(avg(downloads)), count(*)
+from fake_apps
+group by price
+having count(*) > 10;
